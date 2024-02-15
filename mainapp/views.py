8546bc7b5314e3c12def7cdab05fc964277795a2
Yanis37 <136575309+YanisMikulskis@users.noreply.github.com>
2024-02-15 21:45:15 +0300

    Lesson 3 (#2)
    * lesson_3 Template's context
    
    * lesson_3 Template inhereting
    
    * lesson_3 Templates including
    
    * lesson_3 Loops in templates
    
    * lesson_3 Conditions in templates
    
    * lesson_3 Tag with
    
    * lesson_3 Tag static
    
    * lesson_3 New context processor
    
    * lesson_3 Namespaces and urls
    
    * lesson_3 Args in url
    
    * lesson_3 Paginator correct
    
    * lesson_3 upgrade news cards

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from typing import Any
+
 from django.views.generic import TemplateView
 
 
@@ -7,6 +10,16 @@ class MainPageView(TemplateView):
 
 class NewsPageView(TemplateView):
     template_name = "mainapp/news.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["news_title"] = "Zagolovok_test"
+        context["news_preview"] = "Opisanie_test"
+        context["range"] = range(1, 6)
+        context["datetime_obj"] = datetime.now()
+        context["test_title"] = "Test_zagolovok"
+        context["test_preview"] = "Test_opisanie"
+        return context
 
 
 class CoursesPageView(TemplateView):
@@ -23,3 +36,17 @@ class DocSitePageView(TemplateView):
 
 class LoginPageView(TemplateView):
     template_name = "mainapp/login.html"
+
+
+class TestPageView(TemplateView):
+    template_name = "mainapp/test_html.html"
+
+
+class NewsWithPaginatorView(NewsPageView):
+    def get_context_data(self, page, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(page=page, **kwargs)
+        context["page_num"] = page
+        # context["range_pag"] = range(1,6) if page == 1 else range(6, 12)
+        dict_page = {1: range(1, 6), 2: range(6, 11), 3: range(11, 16), 4: range(16, 21), 5: range(21, 23)}
+        context["range_pag"] = dict_page[page]
+        return context
