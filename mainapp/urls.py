597022c908a93da2bc0fe80af41818b3d56a8778
Yanis Mikulskis <www.titan.ru95@mail.ru>
2024-03-17 21:07:53 +0300

    Корректировка страницы курсов, создание дополнительных миграций

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -10,7 +10,10 @@ urlpatterns = [
     path("news/", views.NewsPageView.as_view(), name="news_page"),
     path("news/<int:page>/", views.NewsWithPaginatorView.as_view(), name="news_paginator"),
     path("page/<int:pk>/", views.NewPageDetailView.as_view(), name="detail_news"),
+
     path("courses/", views.CoursesPageView.as_view(), name="courses_page"),
+    path("courses/<int:pk>/", views.CoursesDetailView.as_view(), name = "courses_details"),
+
     path("contacts/", views.ContactsPageView.as_view(), name="contacts_page"),
     path("doc_site/", views.DocSitePageView.as_view(), name="doc_page"),
     path("login/", views.LoginPageView.as_view(), name="login_page"),
--- mainapp/views.py
+++ mainapp/views.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from typing import Any
 from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView
-from .models import News
+from .models import News, Courses, Lesson, CourseTeachers
 
 class MainPageView(TemplateView):
     template_name = "mainapp/index.html"
@@ -14,21 +14,25 @@ class NewsPageView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['news_qs'] = News.objects.all()[:6]
-        #
-        #
-        #
-        # context["news_title"] = "Zagolovok_test"
-        # context["news_preview"] = "Opisanie_test"
-        # # context["page_number"] = page
-        # #context["first_range"] = range(1, 6)
-        # context["datetime_obj"] = datetime.now()
-        # context["test_title"] = "Test_zagolovok"
-        # context["test_preview"] = "Test_opisanie"
         return context
 
 
 class CoursesPageView(TemplateView):
     template_name = "mainapp/courses_list.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['courses_qs'] = Courses.objects.all()
+        return context
+
+class CoursesDetailView(TemplateView):
+    template_name = "mainapp/courses_detail.html"
+    def get_context_data(self,pk=None, **kwargs):
+        context = super().get_context_data(pk=pk, **kwargs)
+        context["new_object_courses"] = get_object_or_404(Courses, pk=pk)
+        context["lessons"] = Lesson.objects.filter(course=context["new_object_courses"])
+        context["teachers"] = CourseTeachers.objects.filter(course = context["new_object_courses"])
+        context["current_page"] = CoursesPageView.as_view()
+        return context
 
 
 class ContactsPageView(TemplateView):
@@ -50,23 +54,19 @@ class TestPageView(TemplateView):
 class NewsWithPaginatorView(NewsPageView):
     page_by_class = 0
     def get_context_data(self, page, **kwargs: Any) -> dict[str, Any]:
-        NewsWithPaginatorView.page_by_class = page
+        NewsWithPaginatorView.page_by_class = page #введено для того, чтобы при подробном нажатии на кнопку "Назад" мы
+        #возвращались на текущую страницу
         context = super().get_context_data(page=page, **kwargs)
-
         context["page_number"] = page
-
         dict_page = {1: range(1, 6), 2: range(6, 11), 3: range(11, 16), 4: range(16, 21), 5: range(21, 25)}
         context["previous_page"] = page - 1 if page > 1 else list(dict_page.keys())[-1]
         context["next_page"] = page + 1 if page < 5 else list(dict_page.keys())[0]
         context["range_for_news"] = dict_page[page]
         context["paginator_range"] = range(1, 6)
         return context
-
     @classmethod
-    def met(cls):
+    def back(cls):
         return cls.page_by_class
-
-
 
 
 
@@ -74,7 +74,6 @@ class NewPageDetailView(TemplateView):
     template_name = 'mainapp/news_detail.html'
     def get_context_data(self, pk=None, **kwargs):
         context = super().get_context_data(pk=pk, **kwargs)
-        current = pk
         context['news_object'] = get_object_or_404(News, pk=pk)
-        context['current_page'] = NewsWithPaginatorView.met()
+        context['current_page'] = NewsWithPaginatorView.back()
         return context
