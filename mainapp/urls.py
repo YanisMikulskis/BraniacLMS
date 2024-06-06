16d7e3dd17a083186c3eba76fa185cc349523ae1
Yanis Mikulskis <www.titan.ru95@mail.ru>
2024-06-06 19:36:56 +0300

    merge urls mainapp

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -9,12 +9,6 @@ urlpatterns = [
     path("", views.MainPageView.as_view(), name="main_page"),
     path("news/", views.NewsPageView.as_view(), name="news_page"),
     path("news/<int:page>/", views.NewsWithPaginatorView.as_view(), name="news_paginator"),
-<<<<<<< HEAD
-    path("courses/", views.CoursesPageView.as_view(), name="courses_page"),
-    path("contacts/", views.ContactsPageView.as_view(), name="contacts_page"),
-    path("doc_site/", views.DocSitePageView.as_view(), name="doc_page"),
-    path("login/", views.LoginPageView.as_view(), name="login_page"),
-=======
     path("page/<int:pk>/", views.NewPageDetailView.as_view(), name="detail_news"),
 
     path("courses/", views.CoursesPageView.as_view(), name="courses_page"),
@@ -22,6 +16,6 @@ urlpatterns = [
 
     path("contacts/", views.ContactsPageView.as_view(), name="contacts_page"),
     path("doc_site/", views.DocSitePageView.as_view(), name="doc_page"),
->>>>>>> lesson_5
     path("test_page/", views.TestPageView.as_view(), name="test_page_for_namespace"),
 ]
+
