a16572fdde25c2fdeb7fa2e036f4c58d7f665fab
Yanis Mikulskis <www.titan.ru95@mail.ru>
2024-06-06 19:39:40 +0300

    merge view

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
-from datetime import datetime
-from typing import Any
-
-=======
 import os
 from datetime import datetime
 from typing import Any
 from django.shortcuts import get_object_or_404
->>>>>>> lesson_5
 from django.views.generic import TemplateView
 from .models import News, Courses, Lesson, CourseTeachers
 from django.http import HttpResponse
@@ -22,16 +16,6 @@ class NewsPageView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-<<<<<<< HEAD
-        context["news_title"] = "Zagolovok_test"
-        context["news_preview"] = "Opisanie_test"
-        context["range"] = range(1, 6)
-        context["datetime_obj"] = datetime.now()
-        context["test_title"] = "Test_zagolovok"
-        context["test_preview"] = "Test_opisanie"
-        return context
-
-=======
         context['news_qs'] = News.objects.all()[:6]
         return context
 
@@ -65,7 +49,6 @@ class NewsWithPaginatorView(NewsPageView):
     def back(cls):
         return cls.page_by_class
 
->>>>>>> lesson_5
 
 class CoursesPageView(TemplateView):
     template_name = "mainapp/courses_list.html"
@@ -75,46 +58,3 @@ class CoursesPageView(TemplateView):
         context['Courses'] = Courses.objects.all()
         return context
 
-
-class CoursesDetailView(TemplateView):
-    template_name = "mainapp/courses_detail.html"
-
-    def get_context_data(self, pk=None, **kwargs):
-        context = super().get_context_data(pk=pk, **kwargs)
-        context["Courses"] = get_object_or_404(Courses, pk=pk)
-        context["Teachers"] = CourseTeachers.objects.filter(course=context["Courses"])
-        context["Lesson"] = Lesson.objects.filter(course=context["Courses"])
-        return context
-
-
-class ContactsPageView(TemplateView):
-    template_name = "mainapp/contacts.html"
-
-
-class DocSitePageView(TemplateView):
-    template_name = "mainapp/doc_site.html"
-
-
-class LoginPageView(TemplateView):
-<<<<<<< HEAD
-    template_name = "mainapp/login.html"
-=======
-    template_name = "authapp/templates/registration/login.html"
->>>>>>> lesson_5
-
-
-class TestPageView(TemplateView):
-    template_name = "mainapp/test_html.html"
-<<<<<<< HEAD
-
-
-class NewsWithPaginatorView(NewsPageView):
-    def get_context_data(self, page, **kwargs: Any) -> dict[str, Any]:
-        context = super().get_context_data(page=page, **kwargs)
-        context["page_num"] = page
-        # context["range_pag"] = range(1,6) if page == 1 else range(6, 12)
-        dict_page = {1: range(1, 6), 2: range(6, 11), 3: range(11, 16), 4: range(16, 21), 5: range(21, 23)}
-        context["range_pag"] = dict_page[page]
-        return context
-=======
->>>>>>> lesson_5
