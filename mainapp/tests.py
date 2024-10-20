4c6aa10ba844b88a6b608e7547d9eb2f7e3384bf
Yanis Mikulskis <www.titan.ru95@mail.ru>
2024-10-20 22:07:11 +0300

    Настраиваю тесты

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -1,1 +1,17 @@
-# Create your tests here.
+from http import HTTPStatus
+
+from django.test import TestCase
+from django.urls import reverse
+
+
+class TestMainPage(TestCase):
+    def test_page_open(self):
+        path = reverse('mainapp_namespace:main_page')# Create your tests here.
+        result = self.client.get(path)
+        self.assertEqual(result.status_code, HTTPStatus.OK)
+
+class TestNewsPage(TestCase):
+    def test_page_open(self):
+        path = reverse('mainapp_namespace:news_list')
+        result = self.client.get(path)
+        self.assertEqual(result.status_code, HTTPStatus.OK)
