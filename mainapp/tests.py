d927cf3b6351322e15f719760764494e4f21809d
Yanis Mikulskis <www.titan.ru95@mail.ru>
2024-10-23 13:49:40 +0300

    Настраиваю тест аутентификации

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -9,9 +9,10 @@ class TestMainPage(TestCase):
         path = reverse('mainapp_namespace:main_page')# Create your tests here.
         result = self.client.get(path)
         self.assertEqual(result.status_code, HTTPStatus.OK)
-
+        print(f'all ok')
 class TestNewsPage(TestCase):
     def test_page_open(self):
         path = reverse('mainapp_namespace:news_list')
         result = self.client.get(path)
         self.assertEqual(result.status_code, HTTPStatus.OK)
+        print(f'a;; ok 2')
